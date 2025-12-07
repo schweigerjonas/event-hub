aa06eeeee240f8489bbd1e349e305e7010273fde
Jonas Schweiger <jonas.schweiger@outlook.de>
2025-12-07 11:53:46 +0100

    feat: add handler for toggling active status of user account

--- src/main/java/de/othr/event_hub/controller/AdminController.java
+++ src/main/java/de/othr/event_hub/controller/AdminController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import de.othr.event_hub.model.User;
@@ -32,6 +33,19 @@ public class AdminController {
     public String deleteUserFromAdminDashboard(@PathVariable String id) {
         User user = userService.getUserById(Long.valueOf(id));
         userService.deleteUser(user);
+
+        return "redirect:/admin";
+    }
+
+    @PatchMapping("/admin/{id}")
+    public String toggleUserActiveStatusFromAdminDashboard(@PathVariable String id) {
+        User user = userService.getUserById(Long.valueOf(id));
+
+        // toggle "active" status for user
+        Integer active = user.getActive() == 0 ? 1 : 0;
+        user.setActive(active);
+
+        userService.updateUser(user);
 
         return "redirect:/admin";
     }
