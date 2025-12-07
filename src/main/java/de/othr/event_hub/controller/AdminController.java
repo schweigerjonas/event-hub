e1e93ed00cb077bc4edd3ffc212dfac5738f3336
Jonas Schweiger <jonas.schweiger@outlook.de>
2025-12-07 11:19:19 +0100

    feat: add handler for deleting user from admin dashboard

--- src/main/java/de/othr/event_hub/controller/AdminController.java
+++ src/main/java/de/othr/event_hub/controller/AdminController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import de.othr.event_hub.model.User;
 import de.othr.event_hub.service.UserService;
@@ -26,4 +28,11 @@ public class AdminController {
         return "admin/admin-dashboard";
     }
 
+    @DeleteMapping("/admin/{id}")
+    public String deleteUserFromAdminDashboard(@PathVariable String id) {
+        User user = userService.getUserById(Long.valueOf(id));
+        userService.deleteUser(user);
+
+        return "redirect:/admin";
+    }
 }
