e2cf94db855f081ef4178300b43a2ce92e20243d
Jonas Schweiger <jonas.schweiger@outlook.de>
2025-12-07 10:19:33 +0100

    feat: add table of users to admin dashboard

--- src/main/java/de/othr/event_hub/controller/AdminController.java
+++ src/main/java/de/othr/event_hub/controller/AdminController.java
@@ -1,12 +1,28 @@
 package de.othr.event_hub.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import de.othr.event_hub.model.User;
+import de.othr.event_hub.service.UserService;
 
 @Controller
 public class AdminController {
+    private UserService userService;
+
+    public AdminController(UserService userService) {
+        super();
+        this.userService = userService;
+    }
+
     @GetMapping("/admin")
-    public String getAdminDashboard() {
+    public String getAdminDashboard(Model model) {
+        List<User> users = userService.getAllUsers();
+        model.addAttribute("users", users);
+
         return "admin/admin-dashboard";
     }
 
