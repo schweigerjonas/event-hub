9bf7b7dc5234a4cb1bc1602fb8b28e81e26815e1
Jonas Schweiger <jonas.schweiger@outlook.de>
2025-12-07 20:53:59 +0100

    feat: add create user form and handler

--- src/main/java/de/othr/event_hub/controller/AdminDashboardController.java
+++ src/main/java/de/othr/event_hub/controller/AdminDashboardController.java
@@ -6,17 +6,24 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import de.othr.event_hub.dto.UserDto;
 import de.othr.event_hub.model.Authority;
 import de.othr.event_hub.model.User;
 import de.othr.event_hub.service.AuthorityService;
 import de.othr.event_hub.service.UserService;
+import de.othr.event_hub.util.UserMapper;
+import jakarta.validation.Valid;
 
 @Controller
 public class AdminDashboardController {
@@ -31,10 +38,38 @@ public class AdminDashboardController {
 
     @GetMapping("/admin")
     public String getAdminDashboard(Model model) {
+        UserDto userDto = new UserDto();
         List<User> users = userService.getAllUsers();
+
+        userDto.setRole("BENUTZER");
+
         model.addAttribute("users", users);
+        model.addAttribute("userDto", userDto);
 
         return "admin/admin-dashboard";
+    }
+
+    @PostMapping("/admin")
+    public String createUserFromAdminDashboard(
+            @ModelAttribute @Valid UserDto userDto,
+            BindingResult result,
+            RedirectAttributes attr,
+            Model model) {
+        if (result.hasErrors()) {
+            System.out.println("Errors: " + result.getAllErrors().toString());
+
+            // TODO: Add attributes with dto values etc and open modal again if there
+            // occured any errors, ideally it should behave like this: Create User -> error
+            // -> get sent to /admin -> automatically reopen create user modal -> have modal
+            // input and validation errors displayed in the modal
+
+            return "redirect:/admin";
+        }
+
+        User user = new UserMapper().toEntity(userDto);
+        userService.saveUser(user, userDto.getRole());
+
+        return "redirect:/admin";
     }
 
     @DeleteMapping("/admin/{id}")
