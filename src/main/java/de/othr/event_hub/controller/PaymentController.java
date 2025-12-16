6dfe90ef13f4dfb2283832e8ed3c84530d12cc82
MartinWeigl125 <martinweigljun@gmail.com>
2025-12-16 17:49:30 +0100

    feat: save payments in DB

--- src/main/java/de/othr/event_hub/controller/PaymentController.java
+++ src/main/java/de/othr/event_hub/controller/PaymentController.java
@@ -1,5 +1,8 @@
 package de.othr.event_hub.controller;
 
+import java.time.LocalDateTime;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +17,14 @@ import com.paypal.api.payments.Links;
 import com.paypal.api.payments.Payment;
 import com.paypal.base.rest.PayPalRESTException;
 
+import de.othr.event_hub.config.AccountUserDetails;
 import de.othr.event_hub.model.Event;
+import de.othr.event_hub.model.enums.PaymentStatus;
 import de.othr.event_hub.service.EventService;
+import de.othr.event_hub.service.PaymentService;
 import de.othr.event_hub.service.PaypalService;
+import de.othr.event_hub.service.UserService;
+
 import org.springframework.web.bind.annotation.PostMapping;
 
 // for paypal API integration compare https://www.youtube.com/watch?v=_eTcseS410E
@@ -26,12 +34,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class PaymentController {
     
     private final EventService eventService;
+    private final PaymentService paymentService;
     private final PaypalService paypalService;
+    private final UserService userService;
 
-    public PaymentController(EventService eventService, PaypalService paypalService) {
+    public PaymentController(EventService eventService, PaymentService paymentService, PaypalService paypalService, UserService userService) {
         super();
         this.eventService = eventService;
+        this.paymentService = paymentService;
         this.paypalService = paypalService;
+        this.userService = userService;
     }
 
     @GetMapping
@@ -47,7 +59,8 @@ public class PaymentController {
         @RequestParam("method") String method, 
         @RequestParam("amount") String amount, 
         @RequestParam("currency") String currency,
-        @RequestParam("description") String description
+        @RequestParam("description") String description,
+        @AuthenticationPrincipal AccountUserDetails details
     ) {
         try {
             String baseURL = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
@@ -63,15 +76,41 @@ public class PaymentController {
         } catch (PayPalRESTException e) {
             e.printStackTrace();
         }
+        // if no approval url found => error
+        // insert Payment into database
+        de.othr.event_hub.model.Payment paymentEntity = new de.othr.event_hub.model.Payment();
+        paymentEntity.setPaypalTransactionId(null);
+        paymentEntity.setAmount(Double.valueOf(amount));
+        paymentEntity.setStatus(PaymentStatus.FAILED);
+        paymentEntity.setTimestamp(LocalDateTime.now());
+        paymentEntity.setUser(userService.getUserByUsername(details.getUsername()));
+        paymentEntity.setEvent(eventService.getEventById(id).get());
+        paymentService.createPayment(paymentEntity);
+
         return new RedirectView("/events/" + id + "/payments/error");
     }
 
     @GetMapping("/success")
     @ResponseBody
-    public String paymentSuccess(@RequestParam("paymentId") String paymentId, @RequestParam("PayerID") String payerId) {
+    public String paymentSuccess(
+        @PathVariable("id") Long id, 
+        @RequestParam("paymentId") String paymentId, 
+        @RequestParam("PayerID") String payerId,
+        @AuthenticationPrincipal AccountUserDetails details
+    ) {
         try {
             Payment payment = paypalService.executePayment(paymentId, payerId);
             if (payment.getState().equals("approved")) {
+                // insert Payment into database
+                de.othr.event_hub.model.Payment paymentEntity = new de.othr.event_hub.model.Payment();
+                paymentEntity.setPaypalTransactionId(paymentId);
+                paymentEntity.setAmount(Double.valueOf(payment.getTransactions().get(0).getAmount().getTotal()));
+                paymentEntity.setStatus(PaymentStatus.COMPLETED);
+                paymentEntity.setTimestamp(LocalDateTime.now());
+                paymentEntity.setUser(userService.getUserByUsername(details.getUsername()));
+                paymentEntity.setEvent(eventService.getEventById(id).get());
+                paymentService.createPayment(paymentEntity);
+
                 return "Show event detail page, user is participant of event";
             }
         } catch (PayPalRESTException e) {
---
+++ src/main/java/de/othr/event_hub/service/PaymentService.java
@@ -0,0 +1,21 @@
+package de.othr.event_hub.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import de.othr.event_hub.model.Payment;
+
+public interface PaymentService {
+    
+    Payment createPayment(Payment payment);
+
+    List<Payment> getAllPayments();
+
+    Optional<Payment> getPaymentById(Long id);
+
+    Payment updatePayment(Payment payment);
+
+    void deletePayment(Payment payment);
+
+    void deleteAllPayments();
+}
