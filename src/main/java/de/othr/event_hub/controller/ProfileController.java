739fdfd3eafa219674712e7070551068764b40fc
Jonas Schweiger <jonas.schweiger@outlook.de>
2026-01-07 13:59:23 +0100

    feat: add profile update page layout

--- src/main/java/de/othr/event_hub/controller/ProfileController.java
+++ src/main/java/de/othr/event_hub/controller/ProfileController.java
@@ -1,13 +1,19 @@
 package de.othr.event_hub.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import de.othr.event_hub.dto.UserDto;
 
 @Controller
 public class ProfileController {
 
     @GetMapping("/profile")
-    public String getProfile() {
+    public String getProfile(Model model) {
+        UserDto userDto = new UserDto();
+
+        model.addAttribute("userDto", userDto);
 
         return "profile/profile";
     }
