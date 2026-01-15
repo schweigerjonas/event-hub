1470741b0334e8efd139b45d0b0e0b62c3770435
Jonas Schweiger <jonas.schweiger@outlook.de>
2026-01-15 21:41:36 +0100

    refactor: specify notification table name for consistency

--- src/main/java/de/othr/event_hub/model/Notification.java
+++ src/main/java/de/othr/event_hub/model/Notification.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +18,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "notifications")
 public class Notification {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
